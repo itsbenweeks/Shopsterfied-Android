807c38699028f2f2699813f3a776955c529b9d8d
Ben Weeks <bweeks@gmail.com>
2016-05-05 21:25:45 -0400

    Initializing repo

---
+++ app/src/main/java/com/itsbenweeks/shopsterfied/MainActivity.java
@@ -0,0 +1,26 @@
+package com.itsbenweeks.shopsterfied;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Load the site
+        mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
